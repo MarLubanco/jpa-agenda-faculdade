25998f580851d0245a8a39f9e694fb2f4fd98c41
Mario Henrique Adaniya <mhadaniya@gmail.com>
2019-10-07 19:20:21 -0300

    :tada: Configurações inicias do JPA

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import entities.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("jpa-lab3-aula");
+
+        EntityManager em = emf.createEntityManager();
+
+        Product p = new Product();
+        p.setName("Tenis");
+        p.setDescription("Tenis de corrida");
+
+
+        em.persist(p);
+
+        em.close();
+        emf.close();
+    }
+
+}
